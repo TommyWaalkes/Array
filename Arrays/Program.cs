f09306036454591c6a04666089b5072cca4df522
Tommy Waalkes <Tommy@grandcircus.co>
2018-01-29 09:58:42 -0500

    Moved Array as a Test

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -11,12 +11,19 @@ namespace Arrays
         static void Main()
         {
             //You can fill an array on one line
-            int[] nums = { 1, 34, 66, 85, 12, 22 };
+            string[] names2 = new string[] { "tommy", "joey", 10.ToString() };
+            Console.WriteLine(names2.ToString());
+            names2[0] = "tommy";
+            names2[1] = "Aaron";
+
+            //Console.WriteLine(names2[11]);
+
+            int[] nums = { 1, 34, 12, 85, 12, 22 };
             int num = nums[2];
             Console.WriteLine(num);
-            string[] names = new string[] { "Tammy", "Billy", "Joey", "Mikey", "Ice T","Phil" };
+            string[] names = new string[] { "Tammy", "Billy", "Joey", "Mikey", "Ice T" };
             Console.WriteLine("For loop Starting!");
-            for(int i = 0; i<names.Length; i++)
+            for (int i = 0; i < names.Length; i++)
             {
                 string name = names[i];
                 Console.WriteLine(name);
@@ -32,14 +39,14 @@ namespace Arrays
             //It goes in the brackets after the datatype
             //Arrays CANNOT shrink or grow
             string[] cities = new string[5];
-          
-            for(int i = 0; i<cities.Length; i++)
+
+            for (int i = 0; i < cities.Length; i++)
             {
                 Console.WriteLine("Please enter a city name for index " + i);
-               // cities[i] = Console.ReadLine();
+                //cities[i] = Console.ReadLine();
             }
             //What happens if I refer to an index with no value in it?
-            for(int i = 0; i<cities.Length; i++)
+            for (int i = 0; i < cities.Length; i++)
             {
                 //Console.WriteLine("City Number " +i+" is " +cities[i]);
             }
@@ -55,8 +62,19 @@ namespace Arrays
             {
                 Console.WriteLine(name);
             }
-            Console.WriteLine(names);
+            Console.WriteLine("Last Element: " + names.Last());
+            Console.WriteLine("First ELement: " + names);
+            Array.Sort(names);
+            
+            Array.Resize(ref names, 3);
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
             Console.WriteLine("For Each Loop Done!");
         }
+
     }
 }
